bf3a06326a985530081d354227478a7a6460f95e
Jake Thrasher <mjakethrasher@gmail.com>
2021-01-21 09:55:50 -0800

    add album td

--- shopping-cart/render-line-items.js
+++ shopping-cart/render-line-items.js
@@ -3,9 +3,13 @@ import { calcItemTotal } from '../utils.js';
 export function renderLineItems(cartItem, record) {
     const tableRow = document.createElement('tr');
 
-    const tdRecord = document.createElement('td');
-    tdRecord.textContent = `${record.artist} "${record.album}"`;
-    tableRow.append(tdRecord);
+    const tdArtist = document.createElement('td');
+    tdArtist.textContent = record.artist;
+    tableRow.append(tdArtist);
+
+    const tdAlbum = document.createElement('td');
+    tdAlbum.textContent = record.album;
+    tableRow.append(tdAlbum);
 
     const tdQuantity = document.createElement('td');
     tdQuantity.textContent = cartItem.quantity;
@@ -15,7 +19,6 @@ export function renderLineItems(cartItem, record) {
     const tdPrice = document.createElement('td');
     tdPrice.textContent = `$${calcItemTotal(cartItem.quantity, record.price)}`;
     tableRow.append(tdPrice);
-
 
     return tableRow;
 }
